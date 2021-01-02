9fd3f76c8750bd7996c671eef6115a4b8e504539
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-03 02:59:19 +0530

    sytling as an object

--- myapp/src/components/counter.jsx
+++ myapp/src/components/counter.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl:'https://picsum.photos/200' 
+        // imageUrl:'https://picsum.photos/200' 
+    }
+    styles = {
+        fontSize: 100,
+        fontWeight:'bold'
     }
     render() {
-        React.createElement('h1')
+        
         return ( <div>
-            <img src={this.state.imageUrl } alt=""/>
-            <span>{ this.formatCount()}</span>
-            <button>increment</button>
+            
+            <span style = {this.styles} className="badge badge-primary m-2">{ this.formatCount()}</span>
+            <button style = {{fontSize:200}} className = "btn btn-secondary btn-sm">increment</button>
         </div>
         )
     }
@@ -21,3 +26,4 @@ class Counter extends Component {
 }
 
 export default Counter
+//we cna'nt use class property bcoz they are reserved key word so what we have instead of this className for seeting any attribute
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Counter from './components/counter'
 
 ReactDOM.render(<Counter/>,document.getElementById('root'))
