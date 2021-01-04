1384b55a97259674e0a74d66a581d23da7be7ee0
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-05 04:42:03 +0530

    creating component

---
+++ myapp/src/components/counters.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class  Counters extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div></div>
+         );
+    }
+}
+ 
+export default Counters;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Counter from './components/counter'
+import Counters from './components/counters'
 
-ReactDOM.render(<Counter/>,document.getElementById('root'))
-
+ReactDOM.render(<Counters/>,document.getElementById('root'))
+//we can compose component together 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
