3f6453c576d42d770ccbfc337028d3ce0ef972af
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-05 21:25:50 +0530

    some dynamically

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import  Person from'./person'
 class App extends Component {
   state = {  }
   render() { 
     return (
       <div className="App">
+        <Person/>
         <h1>Hi, I'm React App</h1>
       </div>
       // React.createElement( 'div', { className : "App" }, React.createElement('h1',null,'hi fella hw the hell are you'))
--- myapp1/src/person.js
+++ myapp1/src/person.js
@@ -1,1 +1,5 @@
-const person
+import React from 'react';
+const person = () => {
+    return <p>I'm a Person and I'm age is {Math.floor(Math.random()*30) }</p>
+}
+export default person
