7251d36b5055eca260949ffc414158cd9096bdbf
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-05 21:34:43 +0530

    stat

--- myapp1/src/person.js
+++ myapp1/src/person.js
@@ -1,5 +1,10 @@
 import React from 'react';
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age } year old</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} year old</p>
+            <p>{props.children}</p>
+        </div>
+        )
 }
 export default person
