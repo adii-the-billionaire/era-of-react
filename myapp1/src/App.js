226b732dd1ba92d9b7017a5001202d646f1e6026
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-06 03:49:28 +0530

    understanding the value

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import person from './person';
-import  Person from'./person'
+import Person from './person';
+
 class App extends Component {
    state = {
         person: [
@@ -10,22 +10,31 @@ class App extends Component {
             {name:'Grey',age:90}
         ]
     }
-  switchHandler = (name) => {
+  switchHandler = (event) => {
     this.setState( {
       person: [
-        { name: name, age: 90 },
-        { name: name, age: 90 },
-        { name: name, age: 90 }
+        { name: event, age: 90 },
+        { name: 'sia', age: 90 },
+        { name: 'tia', age: 90 }
       ]
     })
-    }
+  }
+  nameChangeHandler = ( event ) => {
+ this.setState( {
+      person: [
+        { name: 'mia', age: 90 },
+        { name: 'sia', age: 90 },
+        { name: event.target.value, age: 90 }
+      ]
+    })
+  }
   render() { 
     return (
       <div className="App">
         <button onClick = {()=>{this.switchHandler('mahiyaaaa')}}></button>
         <Person name ={this.state.person[0].name} age={this.state.person[0].age}/>
         <Person name={this.state.person[1].name} age= {this.state.person[1].age} />
-        <Person name={this.state.person[2].name} age={ this.state.person[2].name} click={this.switchHandler}>
+        <Person name={this.state.person[2].name} age={ this.state.person[2].age} click={this.switchHandler} changed = {this.nameChangeHandler}>
           My hobbies:Racing
         </Person>
         <Person name="si" age="90"/>
