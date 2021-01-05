1d94f49eb4435c188acdd0c9221bf067e4dc46f2
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-05 08:40:08 +0530

    yeahp

---
+++ myapp1/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      React.createElement( 'div', { className : "App" }, React.createElement('h1',null,'hi fella hw the hell are you'))
+     );
+  }
+}
+//this is javascript at the end this compiled to this code
+export default App ;
+
