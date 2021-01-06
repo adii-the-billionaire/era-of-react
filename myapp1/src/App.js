e10a13351328e8db66cd34b8f5422d44c76a1dd4
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-06 23:31:13 +0530

    rendering value simple using block method js not in jsx

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -3,67 +3,83 @@ import './App.css';
 import Person from './person';
 
 class App extends Component {
-   state = { 
-        person: [
-            {name:'Mia',age:90},
-            {name:'Shasha',age:90},
-            {name:'Grey',age:90}
-     ],
-     otherState: 'some other value',
-     showPersons:false
-    }
-  switchHandler = (event) => {
+  state = {
+    person: [
+      { name: 'Mia', age: 90 },
+      { name: 'Shasha', age: 90 },
+      { name: 'Grey', age: 90 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+  switchHandler = ( event ) => {
     this.setState( {
       person: [
         { name: event, age: 90 },
         { name: 'sia', age: 90 },
         { name: 'tia', age: 90 }
       ]
-    })
+    } )
   }
   nameChangeHandler = ( event ) => {
- this.setState( {
+    this.setState( {
       person: [
         { name: 'mia', age: 90 },
         { name: 'sia', age: 90 },
         { name: event.target.value, age: 90 }
       ]
-    })
+    } )
   }
-  togglePersonHandler=()=> {
+  togglePersonHandler = () => {
     const doesShow = this.state.showPersons
-    this.setState({showPersons:!doesShow})
+    this.setState( { showPersons: !doesShow } )
   }
-  render() { 
+  render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor:'pointer'
+      cursor: 'pointer'
+    }
+    let person = null
+    if ( this.state.showPersons ) {
+      person = (
+        <div>
+          <Person name={this.state.person[ 0 ].name} age={this.state.person[ 0 ].age} />
+          <Person name={this.state.person[ 1 ].name} age={this.state.person[ 1 ].age} />
+          <Person name={this.state.person[ 2 ].name} age={this.state.person[ 2 ].age} click={this.switchHandler} changed={this.nameChangeHandler}>
+            My hobbies:Racing
+        </Person>
+          <Person name="si" age="90" />
+        </div>
+      )
     }
     return (
       <div className="App">
         <button style={style} onClick={() => { this.switchHandler( 'mahiyaaaa' ) }}>clickmeBaby</button>
-        <button style ={style} onClick = {this.togglePersonHandler}
+        <button style={style} onClick={this.togglePersonHandler}
         > hide me</button>
-        
-        {
-          this.state.showPersons === true ?
-            <div>
-        <Person name ={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age= {this.state.person[1].age} />
-        <Person name={this.state.person[2].name} age={ this.state.person[2].age} click={this.switchHandler} changed = {this.nameChangeHandler}>
-          My hobbies:Racing
-        </Person>
-        <Person name="si" age="90"/>
-            </div> :null
-        }
-
-        <h1>Hi, I'm React App</h1>
+        {person}
       </div>
-      // React.createElement( 'div', { className : "App" }, React.createElement('h1',null,'hi fella hw the hell are you'))
-     );
+      // {
+      //         this.state.showPersons === true ?
+      //           <div>
+      //       <Person name ={this.state.person[0].name} age={this.state.person[0].age}/>
+      //       <Person name={this.state.person[1].name} age= {this.state.person[1].age} />
+      //       <Person name={this.state.person[2].name} age={ this.state.person[2].age} click={this.switchHandler} changed = {this.nameChangeHandler}>
+      //         My hobbies:Racing
+      //       </Person>
+      //       <Person name="si" age="90"/>
+      //           </div> :null
+      //       }
+
+      //       <h1>Hi, I'm React App</h1>
+      //     </div>
+      //     // React.createElement( 'div', { className : "App" }, React.createElement('h1',null,'hi fella hw the hell are you'))
+      //    );
+      // }
+    )
   }
 }
 //this is javascript at the end this compiled to this code
