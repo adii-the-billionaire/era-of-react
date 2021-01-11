f87d1fb7d01b4bb9c26d1ec945d7d6924c300139
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-11 05:34:56 +0530

    doin

--- vanilla/playground.js
+++ vanilla/playground.js
@@ -11,7 +11,28 @@
 // do {
 //   number = Math.floor(Math.random() * 999);
 // } while ( number < 100 )
-  setInterval(function(){   
-    console.log(Math.floor((Math.random()*100)+1)); 
+  // setInterval(function(){   
+  //   console.log(Math.floor((Math.random()*100)+1)); 
 
- }, 1000);
+
+ //here we have to check the value of every prime number 
+ //prime number definition is it remained is when div
+
+//here is how you can use forEach method to describe the nature of prime or not
+// const num = []
+// for ( var i = 0; i < 42; i++ ){
+  
+//    var a= i*i + i + 41
+//   num.push(a)
+// }
+// console.log( num )
+// num.forEach( ( i, index ) => {
+//   var a = index+1
+//   if ( i % a === 0 ) {
+//     return console.log(`the number is not prime at ${a}`)
+//   }
+// })
+//make functional program of above checking this whether number is prime or not
+
+
+
