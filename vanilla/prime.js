8d6d63e488c181e98e019b5c85386ac350cfc5ea
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-12 07:14:56 +0530

    vanilla

---
+++ vanilla/prime.js
@@ -0,0 +1,58 @@
+///here is what you gonna see this let's have fun baby
+// 
+//also we have to value the filter
+// const array = [ [], [], [],[] ]
+
+// const tip = [ ...array[ 0 ] ]
+// for ( var i = 1; i <9; i++ ){
+//     let num = Math.pow( i, 4 )
+//     tip.push(num)
+// }
+// console.log(tip)
+//how to concat multiple array so here is code
+// const a = [0]
+// const b = [1]
+// const c = [2]
+// const d = [ 3 ]
+// const e = []
+// e.push( a,b ,c,d)
+// console.log(e)
+ 
+
+
+
+// const fun1 = () => {
+//    for ( var i = 1; i < 0; i++ ){
+        
+//     }
+// }
+
+// const fun2 = () => {
+//    for ( var i = 1; i < 0; i++ ){
+        
+//     }
+// }
+//here is what i think that
+//num1[i] + num2[i]+num3[i]+num4[i]
+let num = []
+let num1 = []
+let num2 = []
+let num3 = []
+let num4 = []
+for ( var i = 1; i <100; i++ ){
+    num.push( Math.pow( i, 4 ) )
+    num1.push( Math.pow( i, 4 ) )
+    num2.push( Math.pow( i, 4 ) )
+    num3.push(Math.pow(i,4))
+}
+num.push( num1, num2, num3,num4 )
+// console.log( num )
+for ( var t = 0; t < num1.length; num++ ){
+    if ( num1[ i ] + num2[ i ] + num3[ i ] === num[ 4 ] ) {
+        console.log(i)
+    }
+}
+
+
+
+
