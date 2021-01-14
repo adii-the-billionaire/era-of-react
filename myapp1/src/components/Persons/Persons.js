75c92d4ae7b34e5422e249dad638686a7cc18173
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-14 14:12:33 +0530

    understanging of pure component

--- myapp1/src/components/Persons/Persons.js
+++ myapp1/src/components/Persons/Persons.js
@@ -3,7 +3,7 @@ import Person from './Person/person'
 class Persons extends Component{
    shouldComponentUpdate( nextProps, nextState ) {
       console.log( '[Person.js] shouldComponentUpdate' )
-      if ( nextProps.persons !== this.props.persons ) {
+      if ( nextProps.persons !== this.props.persons || nextProps.changed !== this.props.changed || nextProps.clicked !== this.props.clicked) {
          return true
       } else {
          return false
