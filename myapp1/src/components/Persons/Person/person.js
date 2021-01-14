3ab9a429ede570caf6ccfb4e7e7f44480e6c0be6
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-14 14:42:25 +0530

    represent synthetical sugar in different ways

--- myapp1/src/components/Persons/Person/person.js
+++ myapp1/src/components/Persons/Person/person.js
@@ -3,13 +3,13 @@ import classes from './Person.module.css'
 class Person extends Component{
     render() {
         console.log('[Person.js] rendering ....')
-       return ( <div className={classes.Person}>
-            <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} year old</p>
-            <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.Click1} ></input>
-            <button >clickme</button>
-        </div>)
+        return [
+            <p key="i1" onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} year old</p>,
+            <p key="i2" >{this.props.children}</p>,
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} ></input>
+        ]
     }
 }
 export default Person
+
 
