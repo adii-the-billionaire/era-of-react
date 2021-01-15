67ac503be2d66b0390d3adf128c1ddc0bce516d1
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-15 13:37:27 +0530

    using ref for focus

--- myapp1/src/components/Persons/Person/person.js
+++ myapp1/src/components/Persons/Person/person.js
@@ -4,13 +4,22 @@ import Auxilliary from '../../../hoc/Auxilliary'
 import withClass from '../../../hoc/WithClass'
 import PropTypes from 'prop-types'
 class Person extends Component{
+    constructor (props) {
+        super( props )
+        this.inputElementRef = React.createRef()
+    }
+    componentDidMount() {
+        this.inputElementRef.current.focus()
+    }
     render() {
         console.log('[Person.js] rendering ....')
         return (
         <Auxilliary>
         <p key="i1" onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} year old</p>
             <p key="i2" >{this.props.children}</p>
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} ></input>
+                <input key="i3"
+                    type="text"
+                    ref={this.inputElementRef  } onChange={this.props.changed} value={this.props.name} ></input>
         </Auxilliary>
         )
     }
