6f12c1fe5a2fbf2cf75ecf2de1c144db58316faa
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-15 08:52:27 +0530

    styling is back but data is missing

--- myapp1/src/components/Persons/Person/person.js
+++ myapp1/src/components/Persons/Person/person.js
@@ -1,6 +1,7 @@
 import React, { Component,Fragment} from 'react';
 import classes from './Person.module.css'
 import Auxilliary from '../../../hoc/Auxilliary'
+import withClass from '../../../hoc/WithClass'
 class Person extends Component{
     render() {
         console.log('[Person.js] rendering ....')
@@ -13,6 +14,6 @@ class Person extends Component{
         )
     }
 }
-export default Person
+export default withClass(Person,classes.Person)
 
 
