03618d1ece1e87f3b9f23e688ac72eed9f52f8d4
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-21 17:02:18 +0530

    logi

--- suspiria/src/App1.js
+++ suspiria/src/App1.js
@@ -3,18 +3,32 @@ class App extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            answer:'Yes'
+            answer: 'Yes',
+            isLoggedIn:false
         }
     }
+    sia = () => {
+        this.setState({isLoggedIn: true })
+    }
     render() { 
+        let word 
+        if ( !this.state.isLoggedIn ) {
+            word = 'you are log out'
+        } else {
+            word = 'you are login'
+        }
         return ( 
             <div>
                 <h1>Is state important to know
-                {this.state.answer}
-                  </h1>
+                 
+             </h1>
+                <h1>You are log </h1>
+                <button onClick={this.sia}>Button</button>
+                <h1>{ word}</h1>
             </div>
          );
     }
 }
  
 export default App;
+//make an button that shows logged in or log out so here is our code 
