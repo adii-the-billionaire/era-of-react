7b9ecca4a4b9229f84ee347286a0c2f36727c484
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-21 20:35:06 +0530

    checked

--- suspiria/src/list1.js
+++ suspiria/src/list1.js
@@ -7,9 +7,24 @@ class Sia extends Component {
         this.state = {
             person:list
         }
+    this.handler = this.handler.bind(this)
+    }
+    handler(id) {
+        console.log( 'id', id )
+        this.setState( prevState =>{
+            const value = prevState.person.map( ( list ) => {
+                if ( list.id === id ) {
+                    list.completed = !list.completed
+                }
+                return list
+            } )
+            return {
+                person:value
+            }
+        })
     }
     render() {
-        const person =this.state.person.map( ( list ) => <Baby id ={list.id} completed={list.completed} text={list.task }/>)
+        const person = this.state.person.map( ( list ) => <Baby id={list.id} completed={list.completed} text={list.task} tip={this.handler}/>)
      return (
          <div>
              {person}
@@ -18,3 +33,4 @@ class Sia extends Component {
     }
 }
 export default Sia
+
--- suspiria/src/list2.js
+++ suspiria/src/list2.js
@@ -2,7 +2,7 @@ import React from 'react'
 const mia = (props) => {
     return (
         <div>
-            <input type="checkbox" checked={props.completed}></input>
+            <input type="checkbox" checked={props.completed} onChange = {()=>{props.tip(props.id)}}></input>
             <p>{props.text}</p>
         </div>
     )
