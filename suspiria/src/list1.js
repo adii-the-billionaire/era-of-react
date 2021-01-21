826f094bd029bb74afcb0ad5d1e35aeffea6b981
adii-the-billionaire <adiishukla196@gmail.com>
2021-01-21 19:44:06 +0530

    lllllllllll

--- suspiria/src/list1.js
+++ suspiria/src/list1.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 import list from './list'
 import Baby from './list2'
- function Sia() {
-     const person = list.map( ( list ) => <Baby id={list.id} completed={list.completed} text={list.task }/>)
+class Sia extends Component {
+    constructor () {
+        super()
+        this.state = {
+            person:list
+        }
+    }
+    render() {
+        const person =this.state.person.map( ( list ) => <Baby id ={list.id} completed={list.completed} text={list.task }/>)
      return (
          <div>
              {person}
          </div>
     )
+    }
 }
 export default Sia
