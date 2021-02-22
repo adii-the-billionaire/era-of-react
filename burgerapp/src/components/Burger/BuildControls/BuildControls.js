a89db073285568b13113a8a9d3325b6452ab0adb
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-22 23:51:59 +0530

    buildr control

---
+++ burgerapp/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import classes from './BuildControls.module.css'
+const buildControls = ( props ) => {
+    <div className = {classes.BuildControls}>
+    </div>
+}
+export default buildControls
