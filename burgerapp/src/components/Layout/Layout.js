e95706da7649942290ac447309be4a2f760f189a
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-22 18:47:10 +0530

    burger app

---
+++ burgerapp/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.module.css'
+const Layout = ( props ) => {
+    return (
+        <Aux>
+            <div>
+            Toolbar, SideDrawer,Backdrop
+            </div>
+            <main className = {classes.Content}>
+                {props.children}
+            </main>
+        </Aux>
+    )
+}
+export default Layout
