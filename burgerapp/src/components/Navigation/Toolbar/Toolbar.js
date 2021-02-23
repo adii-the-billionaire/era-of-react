4d68be1d73c5175ae4baf04cf1b70d950fe38e92
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-23 20:36:08 +0530

    logo navigation

--- burgerapp/src/components/Navigation/Toolbar/Toolbar.js
+++ burgerapp/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './Toolbar.module.css'
+import Logo from '../../LOGO/Logo'
 const toolbar = ( props ) => {
     return (
         <header className ={classes.Toolbar}>
             <div>MENU</div>
-            <div>LOGO</div>
+            <Logo/>
             <nav>
                 <ul>
                 </ul>
