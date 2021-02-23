3fa149157917623174286305a346d7a498ac75b1
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-23 22:31:13 +0530

    adding navigation to our project

---
+++ burgerapp/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './NavigationItems.module.css'
+import NavigationItem from './NavigationItem/Navigationitem'
+const navigationItems = () => {
+   return(<ul className = {classes.NavigationItems}>
+        <NavigationItem link = "/" active>Burger Builder</NavigationItem>
+        <NavigationItem link ="/">Checkout</NavigationItem>
+    </ul>)
+}
+export default navigationItems      
--- burgerapp/src/components/Navigation/Toolbar/Toolbar.js
+++ burgerapp/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import classes from './Toolbar.module.css'
 import Logo from '../../LOGO/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 const toolbar = ( props ) => {
     return (
         <header className ={classes.Toolbar}>
             <div>MENU</div>
             <Logo/>
             <nav>
-                <ul>
-                </ul>
+                <NavigationItems/>
             </nav>
         </header>
     )
