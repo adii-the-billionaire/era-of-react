10a686b4d234275241b3d80eb2fcb21255bd624a
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-25 16:30:09 +0530

    working in https

---
+++ posthttp/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+class App extends Component {
+  state = {  }
+  render() { 
+    return (  
+      <div className = "App">
+        htpppost
+      </div>
+        
+    );
+  }
+}
+ 
+export default App;
+
+
