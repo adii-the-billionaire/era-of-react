652eb554600082927db4b1c8edf40eb1ac7e32d4
adii-the-billionaire <adiishukla196@gmail.com>
2021-02-28 23:32:53 +0530

    routing with href link

--- posthttp/src/containers/Blog/Blog.js
+++ posthttp/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom'
 import Posts from './Posts/Posts'
 import classes from './Blog.module.css';
-
+import NewPost from './NewPost/NewPost'
  class Blog extends Component {
     render() {
         return (
@@ -11,15 +11,16 @@ import classes from './Blog.module.css';
                     <nav>
                         <ul className={classes.ul}>
                             <li className={classes.li}><a href="/" >Home</a></li>
-                            <li className={classes.li}><a href="/" className={classes.a}>Home</a></li>
-                            <li className={classes.li}><a href="/" className={classes.a}>Home</a></li>
+                            <li className={classes.li}><a href="/new-post" className={classes.a}>NewPost</a></li>
+                            <li className={classes.li}><a href="/m" className={classes.a}>Posts</a></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path = "/m" exact component = {Posts}/>
+                <Route path="/m" exact component={Posts} />
+                <Route path='/new-post' exact component={NewPost}></Route>
             </div>
-        );
+        ); 
     }
 }
 
