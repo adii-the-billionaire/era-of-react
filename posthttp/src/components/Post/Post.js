77a5c9d422d13b73cbada90970605f7451f065c2
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-03 02:28:35 +0530

    routing relate are not pass to the component tree

--- posthttp/src/components/Post/Post.js
+++ posthttp/src/components/Post/Post.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import classes from './Post.module.css';
 
-const post = (props) => (
-    <article className={classes.Post} onClick={props.clicked}>
+const post = ( props ) => {
+    console.log(props)
+    return (<article className={classes.Post} onClick={props.clicked}>
         <h1>{props.title }</h1>
         <div className={classes.Info}>
             <div className={classes.Author} >{ props.author}</div>
         </div>
-    </article>
-);
-
+    </article>)
+    }
 export default post;
