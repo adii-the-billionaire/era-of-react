60360bdcff2cb69d25223e2c7a4c684fb1b6af91
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-07 23:56:24 +0530

    error related order

--- burgerapp/src/containers/Orders/Orders.js
+++ burgerapp/src/containers/Orders/Orders.js
@@ -25,8 +25,9 @@ class Orders extends Component {
     render() { 
         return (
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map( order => (
+                    <Order key={ order.id}/>
+               ))}
             </div>
           );
     }
