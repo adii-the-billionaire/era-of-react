dc75a91f50ddd3c1f9987138e4e7b5f3751aee11
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-08 23:26:39 +0530

    dynamicall rendering input

--- burgerapp/src/containers/Checkout/ContactData/ContactData.js
+++ burgerapp/src/containers/Checkout/ContactData/ContactData.js
@@ -89,12 +89,22 @@ class ContactData extends Component {
 }
 
     render() {
+        const formElementsArray = []
+        for ( let key in this.state.orderForm ) {
+            formElementsArray.push( {
+                id: key,
+                config:this.state.orderForm[key] 
+            })
+        }
         let form = (
              <form>
-                    <Input elementType="" elementConfig="" value=""/>
-                    <Input inputtype="input" type="text" name="email" placeholder="Your email" />
-                    <Input inputtype="input" type="text" name="street" placeholder="Your street"/>
-                    <Input inputtype="input" type="text" name="postalCode" placeholder="Your postalCode" />
+                {formElementsArray.map( formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value }/>
+                ))}
                     <Button btnType = "Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
         )
