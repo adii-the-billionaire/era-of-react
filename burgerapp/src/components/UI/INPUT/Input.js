d0bb17817b59b5ffc35eadca2ce583570c25a09b
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-08 17:50:32 +0530

    intialising form

---
+++ burgerapp/src/components/UI/INPUT/Input.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from './Input.module.css'
+const input = ( props ) => {
+    let inputElement = null
+    switch ( props.inputtype ) {
+        case ( 'input' ):
+            inputElement = <input className={classes.inputElement}  {...props} />
+            break
+        case ( 'textarea' ):
+            inputElement = <textarea className={classes.inputElement} {...props} />
+            break
+        default:
+            inputElement = <input className={classes.inputElement}  {...props}/> 
+    }
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+export default input
+
