8b6d8a9263fdc9df67ce7d335748fc1d163bf166
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-10 01:30:01 +0530

    background color show when touched

--- burgerapp/src/containers/Checkout/ContactData/ContactData.js
+++ burgerapp/src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valdid:false
+                valdid: false,
+                touched:false
             },
 
             street: {
@@ -30,7 +31,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid:false
+                valid: false,
+                touched:false
             },
             
             zipCode: {
@@ -45,7 +47,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength:5
                 },
-                valid:false
+                valid: false,
+                touched:false
                 },
                 
             country: {
@@ -58,7 +61,8 @@ class ContactData extends Component {
                 validation: {
                     required: true, 
                 },
-                valid:false
+                valid: false,
+                touched:false
             },
 
             email: {
@@ -71,7 +75,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid:false
+                valid: false,
+                touched:false
             },
       
             deliveryMethod: {
@@ -86,7 +91,8 @@ class ContactData extends Component {
                      }]
                 },
                 value: '',
-                valid:false
+                valid: false,
+                touched:false
             }
         },
         loading:false
@@ -137,7 +143,8 @@ class ContactData extends Component {
         }
         const updatedFormElement = { ...updatedOrderForm[ inputIdentifier ] }
         updatedFormElement.value = event.target.value
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value,updatedFormElement.validation)
+        updatedFormElement.valid = this.checkValidity( updatedFormElement.value, updatedFormElement.validation )
+        updatedFormElement.touched=true
         updatedOrderForm[ inputIdentifier ] = updatedFormElement
         console.log(updatedFormElement)
         this.setState({orderForm:updatedOrderForm})
@@ -160,7 +167,8 @@ class ContactData extends Component {
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
                         invalid={!formElement.config.valid}
-                        shouldValidate={ formElement.config.validation}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={( event ) => this.inputChangedHandler( event, formElement.id )}
                      />
                 ))}
