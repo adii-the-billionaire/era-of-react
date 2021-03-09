1e02df23b43cd909c2833919dc3b8980fae671be
adii-the-billionaire <adiishukla196@gmail.com>
2021-03-09 20:38:51 +0530

    initialising the value

--- burgerapp/src/components/UI/INPUT/Input.js
+++ burgerapp/src/components/UI/INPUT/Input.js
@@ -4,14 +4,14 @@ const input = ( props ) => {
     let inputElement = null
     switch ( props.elementType ) {
         case ( 'input' ):
-            inputElement = <input className={classes.inputElement}  {...props.elementConfig} value={props.value}/>
+            inputElement = <input onChange={props.changed} className={classes.inputElement}  {...props.elementConfig} value={props.value}/>
             break
         case ( 'textarea' ):
-            inputElement = <textarea className={classes.inputElement} {...props.elementConfig} value={props.value}/>
+            inputElement = <textarea onChange={props.changed}className={classes.inputElement} {...props.elementConfig} value={props.value}/>
             break
         case ( 'select' ):
             inputElement = (
-                <select className={classes.inputElement} value={props.value}>
+                <select onChange={props.changed}className={classes.inputElement} value={props.value}>
                  {props.elementConfig.options.map( option => {
                     return(<option key={option.value} value={option.value}>{ option.displayValue}</option>)
             })}
@@ -19,7 +19,7 @@ const input = ( props ) => {
             )
                 break;
         default:
-            inputElement = <input className={classes.inputElement}  {...props.elementConfig} value={props.value}/>
+            inputElement = <input onChange={props.changed} className={classes.inputElement}  {...props.elementConfig} value={props.value}/>
     }
     return (
         <div className={classes.Input}>
